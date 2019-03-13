66fbd14ab8a4c52f95f62325b3170de72a92d0bd
Mikita Isakau <mikita_Isakau@epam.com>
2019-03-13 11:02:50 +0300

    Add some tests for TryParse

--- NumberParserCore/NumberParserCoreTest/NumberParserTest.cs
+++ NumberParserCore/NumberParserCoreTest/NumberParserTest.cs
@@ -1,8 +1,8 @@
 namespace NumberParserTest
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using NumberParserLib;
     using NumberParserLib.Exceptions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
     public class NumberParserTest
@@ -105,6 +105,111 @@ namespace NumberParserTest
             Assert.AreEqual(NumberParser.Parse("    -9"), -9);
             Assert.AreEqual(NumberParser.Parse("-99    "), -99);
             Assert.AreEqual(NumberParser.Parse("    -9999"), -9999);
+        }
+
+        [TestMethod]
+        public void ShouldFalseIfNull()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse(null, out var i), false);
+        }
+
+        [TestMethod]
+        public void ShouldFalseIfEmptyString()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse("", out var i), false);
+        }
+
+        [TestMethod]
+        public void ShouldFalseIfWrongNumber()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse("12ea", out var i), false);
+        }
+
+        [TestMethod]
+        public void ShouldTryParseNegativeNumbers()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse("-1", out var a), true);            
+            Assert.AreEqual(NumberParser.TryParse("-2", out var b), true);            
+            Assert.AreEqual(NumberParser.TryParse("-40", out var c), true);            
+            Assert.AreEqual(NumberParser.TryParse("-2500", out var d), true);
+
+            Assert.AreEqual(a, -1);
+            Assert.AreEqual(b, -2);
+            Assert.AreEqual(c, -40);
+            Assert.AreEqual(d, -2500);
+        }
+
+        [TestMethod]
+        public void ShouldTryParseZero()
+        {
+            Assert.AreEqual(NumberParser.TryParse("0", out var a), true);
+            Assert.AreEqual(NumberParser.TryParse("-0", out var b), true);
+            Assert.AreEqual(a, 0);
+            Assert.AreEqual(b, 0);
+        }
+
+        [TestMethod]
+        public void ShouldTryParsePositiveNumbersWithPlus()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse("+1", out var a), true);
+            Assert.AreEqual(NumberParser.TryParse("+2", out var b), true);
+            Assert.AreEqual(NumberParser.TryParse("+40", out var c), true);
+            Assert.AreEqual(NumberParser.TryParse("+2500", out var d), true);
+
+            Assert.AreEqual(a, 1);
+            Assert.AreEqual(b, 2);
+            Assert.AreEqual(c, 40);
+            Assert.AreEqual(d, 2500);
+        }
+
+        [TestMethod]
+        public void ShouldTryParsePositiveNumbers()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse("1", out var a), true);
+            Assert.AreEqual(NumberParser.TryParse("2", out var b), true);
+            Assert.AreEqual(NumberParser.TryParse("40", out var c), true);
+            Assert.AreEqual(NumberParser.TryParse("2500", out var d), true);
+
+            Assert.AreEqual(a, 1);
+            Assert.AreEqual(b, 2);
+            Assert.AreEqual(c, 40);
+            Assert.AreEqual(d, 2500);
+        }
+
+        [TestMethod]
+        public void ShouldTryParsePositiveNumbersWithWhitespaces()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse("       1", out var a), true);
+            Assert.AreEqual(NumberParser.TryParse("2        ", out var b), true);
+            Assert.AreEqual(NumberParser.TryParse("       40", out var c), true);
+            Assert.AreEqual(NumberParser.TryParse("     2500       ", out var d), true);
+
+            Assert.AreEqual(a, 1);
+            Assert.AreEqual(b, 2);
+            Assert.AreEqual(c, 40);
+            Assert.AreEqual(d, 2500);
+        }
+
+        [TestMethod]
+        public void ShouldTryParseNegativeNumbersWithWhitespaces()
+        {
+            // Then
+            Assert.AreEqual(NumberParser.TryParse("       -1", out var a), true);
+            Assert.AreEqual(NumberParser.TryParse("-2        ", out var b), true);
+            Assert.AreEqual(NumberParser.TryParse("       -40", out var c), true);
+            Assert.AreEqual(NumberParser.TryParse("     -2500       ", out var d), true);
+
+            Assert.AreEqual(a, -1);
+            Assert.AreEqual(b, -2);
+            Assert.AreEqual(c, -40);
+            Assert.AreEqual(d, -2500);
         }
     }
 }
